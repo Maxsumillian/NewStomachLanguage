405340cbb8e624c2723e3b7246a335dee9cdd3ca
Maximillian Yang <Maxy2002@icloud.com>
2026-04-26 00:47:20 -0400

    fixed some formatting

--- main.cpp
+++ main.cpp
@@ -7,11 +7,15 @@ int main() {
 
 
     Variable test;// creates empty space in memory // Parent Class of all variable class
+
     Calorie test2;// test for ints
     Foods test3;// test for strings
     Edible test4;// test for boolean
+
     Edible test5 = false;// requires constructor
     Foods test6 = "test String";
+    Calorie test7 = 321;
+
 
 
     StomachLang::cpu.load(1);
@@ -39,6 +43,7 @@ int main() {
     test3 = "Wassup";// this also works!
     test4 = false;
 
+    //since its from cpu it returns the encoded values
     //prints out what's stored in my virtual memory
     StomachLang::cpu.load(1);
     StomachLang::cpu.DEVELOPER_Print();
@@ -50,6 +55,15 @@ int main() {
     StomachLang::cpu.DEVELOPER_Print();
 
     StomachLang::cpu.load(4);
+    StomachLang::cpu.DEVELOPER_Print();
+
+    StomachLang::cpu.load(5);
+    StomachLang::cpu.DEVELOPER_Print();
+
+    StomachLang::cpu.load(6);
+    StomachLang::cpu.DEVELOPER_Print();
+
+    StomachLang::cpu.load(7);
     StomachLang::cpu.DEVELOPER_Print();
 
 
