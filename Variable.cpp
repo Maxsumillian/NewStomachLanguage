209d94ce41ee61585d38d4633556abbaf6889f50
Maximillian Yang <Maxy2002@icloud.com>
2026-04-28 03:15:34 -0400

    refactoring getting rid of my local value and type

--- Variable.cpp
+++ Variable.cpp
@@ -10,41 +10,50 @@
 Variable::Variable() {
     maxAddress++;// increments to add to a new location for memory
     address = maxAddress;
-    storeInMemory();
+    storeInMemory("dataType", "dataValue");
 }
 
 Variable::Variable(std::string dataType2, std::string dataValue2) {// only used by virtual cpu to store Temp data
-    dataType = dataType2;
-    dataValue = dataValue2;
-    StomachLang::cpu.store(0);//stores in zero
+    // dataType = dataType2;
+    // dataValue = dataValue2;
+    maxAddress++;
+    address = maxAddress;
+    StomachLang::cpu.add(dataType2 +":"+dataValue2);
+    StomachLang::cpu.store(address);//since this is a temp vairble i can instead create a bool in vairbel to check if is temp and flag it if i want to handle garbage
 }
 
-void Variable::storeInMemory() {// made this virtual because when I create the variable the parent constructor runs first then the child runs but i need a way to make the child run the store function after the parent runs it
+void Variable::storeInMemory(std::string dataType, std::string dataValue) {// made this virtual because when I create the variable the parent constructor runs first then the child runs but i need a way to make the child run the store function after the parent runs it
     StomachLang::cpu.add(dataType +":"+dataValue);//sets data into cpu accumulator
     StomachLang::cpu.store(address);
 }
 
+void Variable::storeInMemory(int address,int address2) {// made this virtual because when I create the variable the parent constructor runs first then the child runs but i need a way to make the child run the store function after the parent runs it
+    StomachLang::cpu.add(address2);//sets data into cpu accumulator
+    StomachLang::cpu.store(address);
+}
 
 //returns variable address in memory
 int Variable::getAddress() {
     return address;
 }
 
-std::string Variable::getType() {
-    return dataType;
-}
+// std::string Variable::getType() {
+//     return dataType;
+// }
 
-std::string Variable::getValue() {
-    return dataValue;
-}
+// std::string Variable::getValue() {
+//     return dataValue;
+// }
 
 // for testing
-void Variable::printTest() {
-    std::cout << "\n the Variable " << dataType << " is located at memory[" << address << "]" << " The MaxAddress to be incremented to is at " << maxAddress << std::endl;
-}
+// void Variable::printTest() {
+//     std::cout << "\n the Variable " << dataType << " is located at memory[" << address << "]" << " The MaxAddress to be incremented to is at " << maxAddress << std::endl;
+// }
 
-Variable Variable::operator+(Variable var2) {
-    StomachLang::cpu.add(dataType +":"+dataValue, var2.getType() + ":" + var2.getValue());// this adds into register but how do i take that and use it here?
+Variable Variable::operator+(Variable var2) {// adjust this instead of grabbing its inhearent values i need to grab from my virtual memory using the addresses from variables
+    //NOtes "getType" is not needed when i transistion to cpu and virtual memory beacause i will use my decoder in my memory
+    StomachLang::cpu.add(address, var2.getAddress());
+    //StomachLang::cpu.add(dataType +":"+dataValue, var2.getType() + ":" + var2.getValue());// this adds into register but how do i take that and use it here?
 
     return StomachLang::cpu.createVariable();
     // std::cout << dataType +":"+dataValue + var2.getType() + ":" + var2.getValue();
@@ -52,7 +61,7 @@ Variable Variable::operator+(Variable var2) {
 
 
 void Variable::operator-(Variable var2){
-    StomachLang::cpu.sub(dataType + ":" + dataValue, var2.getType() + ":" + var2.getValue());
+    StomachLang::cpu.sub(address, var2.getAddress());
     // std::cout << dataType +":"+dataValue + var2.getType() + ":" + var2.getValue();
 }
 
--- Variable.h
+++ Variable.h
@@ -9,9 +9,9 @@
 
 class Variable{// Father of all Variable Types in my Coding langauge
 protected:
-    std::string name;
-    std::string dataType = "dataType";
-    std::string dataValue = "dataValue";
+    // std::string name;
+    // std::string dataType = "dataType";// actually i need to move this to the memory and only keep the address in my variable Class and refrence my cpu to load and display the values i want will require more testing
+    // std::string dataValue = "dataValue";
     int address;// right now address is garbage I need to have a counter that increments the size as my variable numbers grow
     inline static int maxAddress = 0;
 
@@ -26,7 +26,8 @@ public:
     std::string getType();
     std::string getValue();
 
-    virtual void storeInMemory();
+    virtual void storeInMemory(std::string, std::string);
+    virtual void storeInMemory(int address,int address2);
 
     virtual Variable operator+(Variable var2);// returns a Variable so i can chain them
 
