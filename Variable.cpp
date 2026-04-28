11d4d5054da00ea8734652c3c91cef41540a060f
Maximillian Yang <Maxy2002@icloud.com>
2026-04-28 01:08:10 -0400

    added chaining to my add function
    also a construcotr for temporary values created by the cpu to be store at location 0

--- Variable.cpp
+++ Variable.cpp
@@ -13,6 +13,12 @@ Variable::Variable() {
     storeInMemory();
 }
 
+Variable::Variable(std::string dataType2, std::string dataValue2) {// only used by virtual cpu to store Temp data
+    dataType = dataType2;
+    dataValue = dataValue2;
+    StomachLang::cpu.store(0);//stores in zero
+}
+
 void Variable::storeInMemory() {// made this virtual because when I create the variable the parent constructor runs first then the child runs but i need a way to make the child run the store function after the parent runs it
     StomachLang::cpu.add(dataType +":"+dataValue);//sets data into cpu accumulator
     StomachLang::cpu.store(address);
@@ -37,18 +43,22 @@ void Variable::printTest() {
     std::cout << "\n the Variable " << dataType << " is located at memory[" << address << "]" << " The MaxAddress to be incremented to is at " << maxAddress << std::endl;
 }
 
-void Variable::operator+(Variable var2) {
+Variable Variable::operator+(Variable var2) {
     StomachLang::cpu.add(dataType +":"+dataValue, var2.getType() + ":" + var2.getValue());// this adds into register but how do i take that and use it here?
 
+    return StomachLang::cpu.createVariable();
     // std::cout << dataType +":"+dataValue + var2.getType() + ":" + var2.getValue();
 }
 
 
 void Variable::operator-(Variable var2){
-    StomachLang::cpu.sub(dataType +":"+dataValue, var2.getType() + ":" + var2.getValue());
+    StomachLang::cpu.sub(dataType + ":" + dataValue, var2.getType() + ":" + var2.getValue());
     // std::cout << dataType +":"+dataValue + var2.getType() + ":" + var2.getValue();
 }
 
-void Variable::operator=(Variable var2) {
-
-}
+// Variable& Variable::operator=(Variable var2) {
+//     dataType = var2.getType();
+//     dataValue = var2.getValue();
+//     storeInMemory();
+//     return *this;
+// }
