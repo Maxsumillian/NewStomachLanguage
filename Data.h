351b426d302f05dfa57be988fb376ddd41bae754
Maximillian Yang <Maxy2002@icloud.com>
2026-05-01 03:35:16 -0400

    created data struct for my memory

---
+++ Data.h
@@ -0,0 +1,17 @@
+﻿//
+// Created by maxy2 on 5/1/2026.
+//
+
+#ifndef NEWSTOMACHLANGAUGE_DATA_H
+#define NEWSTOMACHLANGAUGE_DATA_H
+
+#pragma once
+#include <string>
+
+struct Data {
+    std::string type;
+    std::string value;
+};
+
+
+#endif //NEWSTOMACHLANGAUGE_DATA_H
